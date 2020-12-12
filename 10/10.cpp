86315e746ea8dee3df3d9e288dd3cf10d8c33ac0
lnarolski <narolskilukasz+github@gmail.com>
2020-12-12 16:59:37 +0100

    Working on Day 10

--- 10/10.cpp
+++ 10/10.cpp
@@ -45,9 +45,64 @@ uint64_t Star1(std::vector<uint64_t>& outputJoltage)
     return differencesOf1Jolt * differencesOf3Jolt;
 }
 
-uint64_t Star2(std::vector<uint64_t> outputJoltage)
+// Source: http://www.algorytm.org/algorytmy-arytmetyczne/symbol-newtona/newton-symbol-2-c.html
+unsigned long long Newton(unsigned int n, unsigned int k)      // Funkcja obliczaj¹ca symbol newtona
 {
-    return 0;
+    double Wynik = 1;       // Deklaracja zmiennej ( u¿ywamy double dla dok³adnoœci obliczeñ )
+
+    for (unsigned int i = 1; i <= k; i++)   // Od 1 do k wykonujemy :
+    {
+        Wynik = Wynik * (n - i + 1) / i;      // Obliczanie ze wzoru iteracyjnego
+    }
+
+    return (unsigned long long) Wynik;      // Zwróæ Wynik
+}
+
+void PrintCombination(std::vector<uint64_t>& outputJoltage)
+{
+    for (size_t i = 0; i < outputJoltage.size(); i++)
+    {
+        std::cout << outputJoltage[i] << " ";
+    }
+
+    std::cout << std::endl;
+}
+
+void CalculateCombinations(uint64_t& combinations, std::vector<uint64_t>& outputJoltage)
+{
+    bool deleted = false;
+    for (size_t i = 1; i < outputJoltage.size() - 1; i++)
+    {
+        if ((outputJoltage[i] - outputJoltage[i - 1] == 1 && outputJoltage[i + 1] - outputJoltage[i] == 1) || (outputJoltage[i] - outputJoltage[i - 1] == 1 && outputJoltage[i + 1] - outputJoltage[i] == 2) || (outputJoltage[i] - outputJoltage[i - 1] == 2 && outputJoltage[i + 1] - outputJoltage[i] == 1))
+        {
+            std::vector<uint64_t> tempOutputJoltage = outputJoltage;
+            tempOutputJoltage.erase(tempOutputJoltage.begin() + i);
+            combinations++;
+            deleted = true;
+
+            //PrintCombination(tempOutputJoltage);
+
+            CalculateCombinations(combinations, tempOutputJoltage);
+        }
+    }
+
+    if (!deleted)
+    {
+        combinations--;
+        //std::cout << "KONIEC" << std::endl;
+    }
+    
+}
+
+uint64_t Star2(std::vector<uint64_t>& outputJoltage)
+{
+    uint64_t combinations = 0;
+
+    outputJoltage.insert(outputJoltage.begin(), 0);
+
+    CalculateCombinations(combinations, outputJoltage);
+
+    return combinations - 1;
 }
 
 int main()
@@ -56,7 +111,7 @@ int main()
     std::sort(outputJoltage->begin(), outputJoltage->end());
 
     std::cout << "Star1: " << Star1(*outputJoltage) << std::endl;
-    std::cout << "Star2: " << Star2(*outputJoltage) << std::endl;
+    //std::cout << "Star2: " << Star2(*outputJoltage) << std::endl;
 
     delete outputJoltage;
 
