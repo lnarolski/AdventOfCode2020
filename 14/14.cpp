a33b96a82a577aeb5c52a1664dc8aece65fa3e9a
lnarolski <narolskilukasz+github@gmail.com>
2020-12-30 18:45:00 +0100

    Working on Day 14

--- 14/14.cpp
+++ 14/14.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <map>
 
 std::vector<std::string>* LoadFile(std::string fileName)
 {
@@ -39,7 +40,41 @@ std::vector<std::string> split(const std::string& str, const std::string& delim)
 
 uint64_t Star1(std::vector<std::string> instructions)
 {
-    return 0;
+    std::map <uint64_t, uint64_t> memory;
+    uint64_t mask[2] = {0};
+
+    for (std::string i : instructions)
+    {
+        if (i.find("mask") != std::string::npos) //mask
+        {
+            std::string maskString[2] = { i.substr(i.find("= ") + 2), i.substr(i.find("= ") + 2) };
+            std::replace(maskString[0].begin(), maskString[0].end(), 'X', '1');
+            std::replace(maskString[1].begin(), maskString[1].end(), 'X', '0');
+
+            mask[0] = stoull(maskString[0], nullptr, 2);
+            mask[1] = stoull(maskString[1], nullptr, 2);
+        }
+        else //mem
+        {
+            uint64_t address = std::stoull(i.substr(4, i.find(']', 4) - 4));
+            uint64_t value = std::stoull(i.substr(i.find("= ") + 2));
+
+            if (memory.find(address) == memory.end())
+            {
+                memory.insert({ address, 0 });
+            }
+            
+            memory[address] = (value | mask[1]) & mask[0];
+        }
+    }
+
+    uint64_t sum = 0;
+    for (auto i : memory)
+    {
+        sum += i.second;
+    }
+
+    return sum;
 }
 
 uint64_t Star2(std::vector<std::string> instructions)
