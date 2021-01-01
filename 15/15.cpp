80bf10d06212caddb8626576d0a8bf942bbfdef3
lnarolski <narolskilukasz+github@gmail.com>
2021-01-01 19:00:58 +0100

    Working on Day 15

---
+++ 15/15.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <bitset>
+
+std::vector<std::string>* LoadFile(std::string fileName)
+{
+    std::vector<std::string>* data = new std::vector<std::string>();
+
+    std::fstream input;
+    input.open(fileName, std::fstream::in);
+    std::string fileLine;
+
+    while (std::getline(input, fileLine))
+    {
+        data->push_back(fileLine);
+    }
+    input.close();
+
+    return data;
+}
+
+std::vector<std::string> split(const std::string& str, const std::string& delim)
+{
+    std::vector<std::string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == std::string::npos) pos = str.length();
+        std::string token = str.substr(prev, pos - prev);
+        tokens.push_back(token);
+
+        prev = pos + delim.length();
+    } while (pos < str.length() && prev < str.length());
+    return tokens;
+}
+
+uint64_t Star1(std::vector<std::string> instructions)
+{
+    return 0;
+}
+
+uint64_t Star2(std::vector<std::string> instructions)
+{
+    return 0;
+}
+
+int main()
+{
+    std::vector<std::string>* data = LoadFile("15.txt");
+
+    std::cout << "Star1: " << Star1(*data) << std::endl;
+    std::cout << "Star2: " << Star2(*data) << std::endl;
+
+    delete data;
+
+    return 0;
+}
