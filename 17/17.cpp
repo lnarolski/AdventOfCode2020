554a56ddb67091520da5a4fbdf007e02582a48cb
lnarolski <narolskilukasz+github@gmail.com>
2021-01-03 13:53:45 +0100

    Working on Day 17

--- 17/17.cpp
+++ 17/17.cpp
@@ -26,17 +26,64 @@ class ThreeDimensionalArea
 {
 public:
     std::vector<std::string> layer;
-    std::list<std::vector<std::string>>* nextLayer = NULL;
-    std::list<std::vector<std::string>>* previousLayer = NULL;
+    ThreeDimensionalArea* nextLayer = NULL;
+    ThreeDimensionalArea* previousLayer = NULL;
 
+    const size_t size = 100;
 
+    ThreeDimensionalArea()
+    {
+        for (size_t i = 0; i < size; ++i)
+        {
+            layer.push_back(std::string(100, '.'));
+        }
+    }
 };
 
 uint64_t Star1(std::vector<std::string> area)
 {
-    std::list<std::vector<std::string>> threeDimensionalArea;
+    ThreeDimensionalArea* zeroLayer = new ThreeDimensionalArea();
+    ThreeDimensionalArea* firstLayer = zeroLayer;
+    ThreeDimensionalArea* lastLayer = zeroLayer;
 
+    // First layer initialisation
+    size_t start = (zeroLayer->size / 2) - 1 - (area[0].size() / 2);
+    size_t i = 0;
+    for (size_t y = start; y < start + 3; ++y)
+    {
+        size_t j = 0;
+        for (size_t x = start; x < start + 3; ++x)
+        {
+            zeroLayer->layer[y][x] = area[i][j];
 
+            ++j;
+        }
+
+        ++i;
+    }
+
+    size_t cycle = 0;
+    ThreeDimensionalArea* actualLayer = zeroLayer;
+    std::vector<ThreeDimensionalArea*> layersToDelete;
+    while (cycle < 6)
+    {
+        // Creating copy
+        ThreeDimensionalArea* tempLayer = firstLayer;
+        do
+        {
+
+        } while (tempLayer != lastLayer);
+
+        for (size_t y = 1; y < actualLayer->layer.size() - 1; ++y)
+        {
+            for (size_t x = 1; x < actualLayer->layer[0].size() - 1; ++x)
+            {
+                actualLayer->layer[y][x];
+            }
+        }
+
+        ++cycle;
+    }
 
     return 0;
 }
