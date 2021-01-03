81db39b68a59287eaa60de655b5fe6c3995e3a36
lnarolski <narolskilukasz+github@gmail.com>
2021-01-03 20:52:13 +0100

    Working on Day 17

--- 17/17.cpp
+++ 17/17.cpp
@@ -40,21 +40,52 @@ public:
     }
 };
 
+void DeleteOldLayers(ThreeDimensionalArea* firstLayer)
+{
+    ThreeDimensionalArea* layerToDelete = firstLayer;
+    do
+    {
+        ThreeDimensionalArea* tempLayer = layerToDelete->nextLayer;
+        delete layerToDelete;
+
+        layerToDelete = tempLayer;
+    } while (layerToDelete != NULL);
+}
+
+uint64_t CountActiveStates(ThreeDimensionalArea* firstLayer)
+{
+    uint64_t activeStates = 0;
+
+    do
+    {
+        for (size_t y = 1; y < firstLayer->layer.size() - 1; ++y)
+        {
+            for (size_t x = 1; x < firstLayer->layer[0].size() - 1; ++x)
+            {
+                if (firstLayer->layer[y][x] == '#')
+                    ++activeStates;
+            }
+        }
+
+        firstLayer = firstLayer->nextLayer;
+    } while (firstLayer != NULL);
+
+    return activeStates;
+}
+
 uint64_t Star1(std::vector<std::string> area)
 {
-    ThreeDimensionalArea* zeroLayer = new ThreeDimensionalArea();
-    ThreeDimensionalArea* firstLayer = zeroLayer;
-    ThreeDimensionalArea* lastLayer = zeroLayer;
+    ThreeDimensionalArea* firstLayer = new ThreeDimensionalArea();
 
     // First layer initialisation
-    size_t start = (zeroLayer->size / 2) - 1 - (area[0].size() / 2);
+    size_t start = (firstLayer->size / 2) - 1 - (area[0].size() / 2);
     size_t i = 0;
     for (size_t y = start; y < start + 3; ++y)
     {
         size_t j = 0;
         for (size_t x = start; x < start + 3; ++x)
         {
-            zeroLayer->layer[y][x] = area[i][j];
+            firstLayer->layer[y][x] = area[i][j];
 
             ++j;
         }
@@ -63,29 +94,103 @@ uint64_t Star1(std::vector<std::string> area)
     }
 
     size_t cycle = 0;
-    ThreeDimensionalArea* actualLayer = zeroLayer;
-    std::vector<ThreeDimensionalArea*> layersToDelete;
     while (cycle < 6)
     {
-        // Creating copy
-        ThreeDimensionalArea* tempLayer = firstLayer;
+        ThreeDimensionalArea* newFirstLayer;
+        ThreeDimensionalArea* newTempLayer = new ThreeDimensionalArea();
+        newFirstLayer = newTempLayer;
+        newFirstLayer->previousLayer = new ThreeDimensionalArea();
+        newFirstLayer->previousLayer->nextLayer = newFirstLayer;
+
+        ThreeDimensionalArea* actualLayer = firstLayer;
+
+        bool stop = false;
         do
         {
-
-        } while (tempLayer != lastLayer);
-
-        for (size_t y = 1; y < actualLayer->layer.size() - 1; ++y)
-        {
-            for (size_t x = 1; x < actualLayer->layer[0].size() - 1; ++x)
+            if (actualLayer->nextLayer == 0)
             {
-                actualLayer->layer[y][x];
+                stop = true;
             }
-        }
+
+            for (size_t y = 1; y < actualLayer->layer.size() - 1; ++y)
+            {
+                for (size_t x = 1; x < actualLayer->layer[0].size() - 1; ++x)
+                {
+                    uint64_t activeNeighborsCount = 0;
+
+                    for (size_t i = y - 1; i <= y + 1; ++i)
+                    {
+                        for (size_t j = x - 1; j <= x + 1; ++j)
+                        {
+                            if (i == y && j == x)
+                                continue;
+
+                            if (actualLayer->layer[i][j] == '#')
+                                ++activeNeighborsCount;
+                        }
+                    }
+
+                    if (actualLayer->previousLayer != NULL)
+                    {
+                        for (size_t i = y - 1; i <= y + 1; ++i)
+                        {
+                            for (size_t j = x - 1; j <= x + 1; ++j)
+                            {
+                                if (actualLayer->previousLayer->layer[i][j] == '#')
+                                    ++activeNeighborsCount;
+                            }
+                        }
+                    }
+                    if (actualLayer->nextLayer != NULL)
+                    {
+                        for (size_t i = y - 1; i <= y + 1; ++i)
+                        {
+                            for (size_t j = x - 1; j <= x + 1; ++j)
+                            {
+                                if (actualLayer->nextLayer->layer[i][j] == '#')
+                                    ++activeNeighborsCount;
+                            }
+                        }
+                    }
+
+                    if (actualLayer->layer[x][y] == '#')
+                    {
+                        if (activeNeighborsCount == 2 || activeNeighborsCount == 3)
+                        {
+                            newTempLayer->layer[x][y] = '#';
+                        }
+                        else
+                        {
+                            newTempLayer->layer[x][y] = '.';
+                        }
+                    }
+                    else
+                    {
+                        if (activeNeighborsCount == 3)
+                        {
+                            newTempLayer->layer[x][y] = '#';
+                        }
+                        else
+                        {
+                            newTempLayer->layer[x][y] = '.';
+                        }
+                    }
+                }
+            }
+
+            actualLayer = actualLayer->nextLayer;
+            newTempLayer->nextLayer = new ThreeDimensionalArea();
+            newTempLayer->nextLayer->previousLayer = newTempLayer;
+            newTempLayer = newTempLayer->nextLayer;
+        } while (!stop);
+
+        DeleteOldLayers(firstLayer);
+        firstLayer = newFirstLayer->previousLayer;
 
         ++cycle;
     }
 
-    return 0;
+    return CountActiveStates(firstLayer);
 }
 
 uint64_t Star2(std::vector<std::string> area)
